68bb6b5ccceecda745fc113419178a2cfb7280aa
eirini-chrysovalanto-efthymiou <ph09144@uoi.gr>
2025-05-21 14:09:18 +0300

    Update myrand2.m

--- myrand2.m
+++ myrand2.m
@@ -5,9 +5,9 @@ endfunction
 
 
 # What does this mean in practice?
-#
+
 # You can close the function file after you save it.
-#
+
 # It is enough to be in the correct folder or have configured MATLAB to "see" the folder containing the file.
-#
+
 # From the Command Window or another script, you can call the function normally.
